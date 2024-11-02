925dd4a8626f078439805c50252ad28dad3fedb6
Taha Khamessi <taha.khamessi@gmail.com>
2024-11-02 17:34:01 +0100

    Pages & components initial Structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Projects from './pages/Projects';
+import Skills from './pages/Skills';
+import Contact from './pages/Contact';
+import GitHubb from './pages/Github';
+import Blog from './pages/Blog';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div>
-      Hello World!
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/skills" element={<Skills />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/github" element={<GitHubb />} />
+        <Route path="/blog" element={<Blog />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/Github.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const GitHub = () => {
+  return <div>GitHub Contributions Page</div>;
+};
+
+export default GitHub;
